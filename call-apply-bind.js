336b14f41818b0a1627b6aeca1f0c63a800cb955
tony <806003097@qq.com>
2022-04-21 23:45:59 +0800

    update

---
+++ call-apply-bind.js
@@ -0,0 +1,25 @@
+Function.prototype.MyCall = function (context, ...args) {
+  context.fn = this;
+  return context.fn(...args);
+};
+
+const obj = {
+  name: "tony",
+};
+function getName(age) {
+  return this.name + ":" + `${age}`;
+}
+
+console.log(getName.MyCall(obj, 28));
+
+// Function.prototype.Mybind = function (context, ...args) {
+//   const fn = this;
+//   return function () {
+//     context.fn = fn;
+//     return context.fn(...args);
+//   };
+// };
+
+// const newGetName = getName.Mybind(obj, 28);
+// const result = newGetName();
+// console.log("result:", result);
