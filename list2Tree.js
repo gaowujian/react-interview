223f701659c3dfac79e4a5ee28ebb1e0f573bb03
tony <806003097@qq.com>
2022-05-09 20:48:05 +0800

    update

---
+++ list2Tree.js
@@ -0,0 +1,118 @@
+let jsonData = [
+  {
+    id: "1",
+    pid: "0",
+    name: "家用电器",
+  },
+  {
+    id: "4",
+    pid: "1",
+    name: "大家电",
+  },
+  {
+    id: "5",
+    pid: "1",
+    name: "生活电器",
+  },
+  {
+    id: "2",
+    pid: "0",
+    name: "服饰",
+  },
+  {
+    id: "3",
+    pid: "0",
+    name: "化妆",
+  },
+  {
+    id: "7",
+    pid: "4",
+    name: "空调",
+  },
+  {
+    id: "8",
+    pid: "4",
+    name: "冰箱",
+  },
+  {
+    id: "9",
+    pid: "4",
+    name: "洗衣机",
+  },
+  {
+    id: "10",
+    pid: "4",
+    name: "热水器",
+  },
+  {
+    id: "11",
+    pid: "3",
+    name: "面部护理",
+  },
+  {
+    id: "12",
+    pid: "3",
+    name: "口腔护理",
+  },
+  {
+    id: "13",
+    pid: "2",
+    name: "男装",
+  },
+  {
+    id: "14",
+    pid: "2",
+    name: "女装",
+  },
+  {
+    id: "15",
+    pid: "7",
+    name: "海尔空调",
+  },
+  {
+    id: "16",
+    pid: "7",
+    name: "美的空调",
+  },
+  {
+    id: "19",
+    pid: "5",
+    name: "加湿器",
+  },
+  {
+    id: "20",
+    pid: "5",
+    name: "电熨斗",
+  },
+];
+
+function list2Tree(arr) {
+  if (!arr || arr.length === 0) return null;
+
+  //   第一层元素
+  const roots = arr.filter((item) => item.pid === "0");
+
+  //  buildTree 的定义 参数是一个树的头节点，返回值是一课构建好的树
+  function buildTree(node) {
+    //  查看该节点能不能作为父亲节点
+    const filtered = arr.filter((item) => item.pid === node.id);
+    if (filtered.length === 0) {
+      return node;
+    }
+    // 说明有儿子节点
+    const children = [];
+    for (let i = 0; i < filtered.length; i++) {
+      children.push(buildTree(filtered[i]));
+    }
+
+    return { ...node, children };
+  }
+  for (let i = 0; i < roots.length; i++) {
+    roots[i] = buildTree(roots[i]);
+  }
+  return roots;
+}
+const result = list2Tree(jsonData);
+console.log("result:", result);
+
+// !实现一个无限层级 数组转树的方法
---
+++ observer.js
@@ -0,0 +1,56 @@
+//目标基类（被观察者）
+class Subject {
+  //一个目标包含自己本身的数据和一个观察者的列表
+  constructor(data) {
+    this.observerList = [];
+    this.data = data;
+  }
+  //向列表中添加观察者（即观察动作的实现）
+  add(obj) {
+    if (arguments.length >= 1) {
+      Array.from(arguments).forEach((item) => this.observerList.push(item));
+    }
+  }
+  //向列表中移除观察者
+  remove(observer) {
+    let i = this.observerList.findIndex((ele) => ele === observer);
+    if (i >= 0) {
+      this.observerList.splice(i, 1);
+    }
+  }
+  //当被观察者本身的数据发生改变时，通知观察者做出相应的改变
+  notify() {
+    //通过调用观察者提供的更新接口来实现更新操作
+    this.observerList.forEach((item) => {
+      item.update(this.data);
+    });
+  }
+}
+
+//观察者基类
+class Observer {
+  constructor(id) {
+    this.id = id;
+  }
+  update(data) {
+    console.log("observer " + this.id + ": " + data + ";");
+  }
+}
+
+//测试观察者模式的函数
+function test() {
+  //创建了一个目标sub 和三个观察者obj1,obj2,obj3
+  let sub = new Subject("test");
+  let ob1 = new Observer(1);
+  let ob2 = new Observer(2);
+  let ob3 = new Observer(3);
+
+  //通过add方法实现“观察”的动作
+  sub.add(ob1, ob2, ob3);
+  sub.notify();
+
+  sub.remove(ob2);
+  sub.notify();
+}
+
+test();
